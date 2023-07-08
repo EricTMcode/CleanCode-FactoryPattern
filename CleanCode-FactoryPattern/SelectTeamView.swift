941d2520ff77733ce65b83c93cbe1b3ae8373ef2
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-08 15:14:11 +0200

    - Implement the logic

--- CleanCode-FactoryPattern/SelectTeamView.swift
+++ CleanCode-FactoryPattern/SelectTeamView.swift
@@ -7,9 +7,79 @@
 
 import SwiftUI
 
+enum PlayerType: String {
+    case pointGard, center
+    
+    var text: String {
+        switch self {
+        case .pointGard:
+            return "Point Guard"
+        case .center:
+            return "Center"
+        }
+    }
+}
+
+protocol Player {
+    var playerType: PlayerType { get }
+    func shoot()
+}
+
+struct Center: Player {
+    let playerType: PlayerType = .center
+    
+    func shoot() {
+        // Logic for shooting center
+        print("Center shoots")
+    }
+}
+
+struct PointGuard: Player {
+    let playerType: PlayerType = .pointGard
+    
+    func shoot() {
+        // Logic for shooting point guard
+        print("Point guard shoots")
+    }
+}
+
+class PlayerFactory {
+    func makePlayer(playerType: PlayerType) -> Player {
+        switch playerType {
+        case .pointGard:
+            return PointGuard()
+        case .center:
+            return Center()
+        }
+    }
+}
+
 struct SelectTeamView: View {
+    let factory = PlayerFactory()
+    @State private var players: [Player] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                ForEach(0..<players.count, id: \.self) { index in
+                    VStack {
+                        Text("\(players[index].playerType.text)")
+                    }
+                }
+            }
+            Spacer()
+            HStack {
+                Button("Point Guard") {
+                    let player = factory.makePlayer(playerType: .pointGard)
+                    players.append(player)
+                }
+                Button("Center") {
+                    let player = factory.makePlayer(playerType: .center)
+                    players.append(player)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
     }
 }
 
