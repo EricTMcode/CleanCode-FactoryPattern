442cc36f5950ac1d5b2e718370f0a59729484139
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-08 15:02:40 +0200

    - Create Project

---
+++ CleanCode-FactoryPattern/SelectTeamView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectTeamView.swift
+//  CleanCode-FactoryPattern
+//
+//  Created by Eric on 08/07/2023.
+//
+
+import SwiftUI
+
+struct SelectTeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectTeamView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectTeamView()
+    }
+}
