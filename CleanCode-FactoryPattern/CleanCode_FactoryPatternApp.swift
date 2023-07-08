05209b95292dbb40f366d013b15b462d6293a68b
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-07-08 15:01:13 +0200

    Initial Commit

---
+++ CleanCode-FactoryPattern/CleanCode_FactoryPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanCode_FactoryPatternApp.swift
+//  CleanCode-FactoryPattern
+//
+//  Created by Eric on 08/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CleanCode_FactoryPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
